1b3ad1f20cbd31e778dd5a7eb6e299eb55a55ea4
gusbe <gusbertolo@gmail.com>
2019-05-22 20:09:52 +0200

    Created first distance search

--- routes/search.js
+++ routes/search.js
@@ -5,9 +5,30 @@ const Location = require('./../models/location.js');
 
 
 
+//GET /search/?coords&dist
+router.get('/', (req, res) => {
+  
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+  const dist = req.query.dist;
+
+  Location.find({
+    coords: {
+      $geoWithin: {
+        $centerSphere: [[lon, lat], dist / 6371000]
+      }
+    }
+  })
+    .then( (results) => {
+      console.log("Results: " + results);
+      res
+        .status(200)
+        .json(results)
+    })
+    .catch( (err) => console.log(err) );
 
 
-
+});
 
 
 
