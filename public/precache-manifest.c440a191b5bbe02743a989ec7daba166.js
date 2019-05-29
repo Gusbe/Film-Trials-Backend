7403f48f4a65daed6bf96f27f00f0e55c073ba53
gusbe <gusbertolo@gmail.com>
2019-05-29 13:23:05 +0200

    To heroku

---
+++ public/precache-manifest.c440a191b5bbe02743a989ec7daba166.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ce2414ca6e702dd1135",
+    "url": "/static/js/main.8ce2414c.chunk.js"
+  },
+  {
+    "revision": "4ba4c77a2f310705dc5f",
+    "url": "/static/js/1.4ba4c77a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d452d5755c691b6bedce07b17b914b18",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2324066b07b49da02bd09ad9f3e90687.js"
+  "/precache-manifest.c440a191b5bbe02743a989ec7daba166.js"
 );
 
 workbox.clientsClaim();
